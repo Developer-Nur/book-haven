47bbe263007f9f194538add94a3daaaad09df688
Developer Nur <nursweb6@gmail.com>
2024-03-28 04:11:38 +0600

    Custom Shape Bar Chart integrated.

--- src/cmponents/ListBooks/ListBooks.jsx
+++ src/cmponents/ListBooks/ListBooks.jsx
@@ -1,4 +1,5 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { AiOutlineDown } from "react-icons/ai";
 import 'react-tabs/style/react-tabs.css';
 import ReadingList from '../ReadingList/ReadingList';
 import WishList from '../WishList/WishList';
@@ -9,7 +10,22 @@ const ListBooks = () => {
             <section className="h-[100px] border-color rounded pt-[20px] mb-10">
                 <h3 className="text-[40px] font-bold prim-title text-center">Books</h3>
             </section>
-            <a className="btn theme-bg text-white text-xl font-semibold ">Sort BY</a>
+
+
+
+            {/* Dropdown */}
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn m-1 theme-bg text-white text-xl font-semibold">
+                    Sort By <AiOutlineDown />
+                </div>
+                <ul tabIndex={0} className="dropdown-content theme-bg text-white text-xl font-semibold menu p-2 shadow  rounded-box w-[300px]">
+                    <li className='border-b-2'><a>Rating</a></li>
+                    <li className='border-b-2'><a>Number of pages</a></li>
+                    <li className='border-b-2'><a>Published year</a></li>
+                </ul>
+            </div>
+
+
             <div className='mt-12 text-left'>
                 <Tabs >
                     <TabList>
--- src/cmponents/PagesToRead/PagesToRead.jsx
+++ src/cmponents/PagesToRead/PagesToRead.jsx
@@ -1,11 +1,52 @@
-
+import { useEffect, useState } from 'react';
+// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const PagesToRead = () => {
+
+    const [readData, setReadData] = useState({});
+
+   const {totalPages, bookName} = readData;
+
+   console.log(totalPages, bookName)
+
+    useEffect(()=>{
+        const dataFromLocalStore = JSON.parse(localStorage.getItem('read'));
+        if(dataFromLocalStore){
+            setReadData(dataFromLocalStore);
+        }
+    },[])
+
+
     return (
         <div>
-            <h2>All pages will be here</h2>
+            <h2>The charts is getting ready</h2>
+            
         </div>
     );
 };
 
 export default PagesToRead;
+
+
+
+
+{/* <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                    ))}
+                </Bar>
+            </BarChart> */}
