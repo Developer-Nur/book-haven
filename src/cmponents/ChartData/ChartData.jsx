cbd42b34e2932386fe189480197786e458a228da
Developer Nur <nursweb6@gmail.com>
2024-03-28 14:59:57 +0600

    Custom Shape Bar Chart created.

---
+++ src/cmponents/ChartData/ChartData.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+  
+  const TriangleBar = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const { fill, x, y, width, height } = props;
+  
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+const ChartData = ({ data }) => {
+
+    return (
+        <div className="chart-container">
+            <BarChart
+                width={1000}
+                height={400}
+                data={data}
+                margin={{
+                    top: 20,
+                    
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bookName" />
+                <YAxis />
+                <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                    ))}
+                </Bar>
+            </BarChart>
+        </div>
+    );
+};
+
+ChartData.propTypes = {
+    data: PropTypes.array.isRequired
+}
+
+export default ChartData;
--- src/cmponents/PagesToRead/PagesToRead.jsx
+++ src/cmponents/PagesToRead/PagesToRead.jsx
@@ -1,26 +1,22 @@
 import { useEffect, useState } from 'react';
-// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import ChartData from '../ChartData/ChartData';
 
 const PagesToRead = () => {
 
-    const [readData, setReadData] = useState({});
+    const [readData, setReadData] = useState([]);
 
-   const {totalPages, bookName} = readData;
 
-   console.log(totalPages, bookName)
-
-    useEffect(()=>{
+    useEffect(() => {
         const dataFromLocalStore = JSON.parse(localStorage.getItem('read'));
-        if(dataFromLocalStore){
+        if (dataFromLocalStore) {
             setReadData(dataFromLocalStore);
         }
-    },[])
+    }, [])
 
 
     return (
-        <div>
-            <h2>The charts is getting ready</h2>
-            
+        <div className='mt-10'>
+            <ChartData data={readData}> </ChartData>
         </div>
     );
 };
@@ -30,23 +26,3 @@ export default PagesToRead;
 
 
 
-{/* <BarChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 20,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                    ))}
-                </Bar>
-            </BarChart> */}
